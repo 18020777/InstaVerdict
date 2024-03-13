bcf8509954be7e090d3faea73360dd171b7fbf11
https://18020777@github.com <selim.dleyssac@protonmail.com>
2024-03-12 19:43:01 -0700

    US3 Loaders added

--- Components/Poll.js
+++ Components/Poll.js
@@ -1,87 +1,89 @@
-import {Button, StyleSheet, Text, View} from "react-native";
+import {ActivityIndicator, Button, StyleSheet, Text, View} from "react-native";
 import {Colors} from "../Utils/Colors";
 import {useEffect, useState} from "react";
 import {Storage} from "../Utils/Storage";
 import {Endpoints} from "../Utils/Endpoints";
 
 const Poll = ({poll}) => {
-    const [vote, setVote] = useState()
-    const [isLoading, setIsLoading] = useState(false)
+	const [vote, setVote] = useState()
+	const [isLoading, setIsLoading] = useState(false)
 
-    const checkVote = async () => setVote(Number(await Storage.get("poll-" + poll.id)))
+	const checkVote = async () => setVote(Number(await Storage.get("poll-" + poll.id)))
 
-    const addVote = async (index) => {
-        setIsLoading(true)
-        try {
-            const votes = poll.votes
-            votes[index] += 1
-            const res = await fetch(Endpoints.vote(poll.id), {
-                method: "PATCH",
-                body: JSON.stringify({votes: votes}),
-                headers: {
-                    "Content-Type": "application/merge-patch+json",
-                }
-            })
-            if (res.ok) {
-                await Storage.set("poll-" + poll.id, index.toString())
-                setVote(index)
-            }
-        } catch (e) {
-            console.log(e)
-        }
-        setIsLoading(false)
-    }
+	const addVote = async (index) => {
+		setIsLoading(true)
+		try {
+			const votes = poll.votes
+			votes[index] += 1
+			const res = await fetch(Endpoints.vote(poll.id), {
+				method: "PATCH",
+				body: JSON.stringify({votes: votes}),
+				headers: {
+					"Content-Type": "application/merge-patch+json",
+				}
+			})
+			if (res.ok) {
+				setVote(index)
+				await Storage.set("poll-" + poll.id, index.toString())
+			}
+		} catch (e) {
+			console.log(e)
+		}
+		setIsLoading(false)
+	}
 
-    useEffect(() => {
-        checkVote()
-    }, [])
+	useEffect(() => {
+		checkVote()
+	}, [])
 
-    return (
-        <View style={styles.questionContainer}>
-            <Text style={styles.question}>{poll.question}</Text>
-            {!isLoading && vote !== undefined && poll.options.map((option, i) => vote !== null ? (
-                <View key={i} style={{...styles.vote, ...(i === vote ? styles.voted : styles.unvoted)}}>
-                    <Text>{option}</Text>
-                    <Text>{poll.votes[i]}</Text>
-                </View>
-            ) : (
-                <Button key={i} title={option} color={Colors.primary} onPress={() => addVote(i)}/>
-            ))}
-        </View>
-    )
+	return (
+		<View style={styles.questionContainer}>
+			<Text style={styles.question}>{poll.question}</Text>
+			{isLoading ? (
+				<ActivityIndicator style={{marginVertical: 10}} color={Colors.primary} />
+			) : vote !== undefined && poll.options.map((option, i) => vote !== null ? (
+				<View key={i} style={{...styles.vote, ...(i === vote ? styles.voted : styles.unvoted)}}>
+					<Text>{option}</Text>
+					<Text>{poll.votes[i]}</Text>
+				</View>
+			) : (
+				<Button key={i} title={option} color={Colors.primary} onPress={() => addVote(i)}/>
+			))}
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    questionContainer: {
-        padding: 10,
-        borderColor: Colors.outline,
-        borderWidth: 1,
-        borderRadius: 10,
-        width: "90%",
-        display: "flex",
-        flexDirection: "column",
-        gap: 10,
-    },
-    question: {
-        fontSize: 15,
-        fontWeight: "bold"
-    },
-    vote: {
-        width: "100%",
-        padding: 10,
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        borderRadius: 5,
-    },
-    unvoted: {
-        borderColor: Colors.outline + "AA",
-        borderWidth: 1,
-    },
-    voted: {
-        backgroundColor: Colors.primary + "55"
-    }
+	questionContainer: {
+		padding: 10,
+		borderColor: Colors.outline,
+		borderWidth: 1,
+		borderRadius: 10,
+		width: "90%",
+		display: "flex",
+		flexDirection: "column",
+		gap: 10,
+	},
+	question: {
+		fontSize: 15,
+		fontWeight: "bold"
+	},
+	vote: {
+		width: "100%",
+		padding: 10,
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		borderRadius: 5,
+	},
+	unvoted: {
+		borderColor: Colors.outline + "AA",
+		borderWidth: 1,
+	},
+	voted: {
+		backgroundColor: Colors.primary + "55"
+	}
 })
 
 export default Poll
